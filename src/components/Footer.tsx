8c81e9a3b589b404dc774f65369828757384a8f1
Gulia <gulya.isaeva.pls@gmail.com>
2024-08-09 14:46:06 +0300

    feat:add slider,offer,feautrued componenets

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
+import Image from "next/image";
 import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between">
+      <Link className="font-bold text-xl" href="/">
+        Gussimo
+      </Link>
+      <p>&copy; ALL RIGHTS RESERVED.</p>
+    </div>
+  );
 }
 
 export default Footer;
