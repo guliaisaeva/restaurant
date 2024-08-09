90687e955c6d86ca7aefa8bfb649ea7356ad6118
Gulia <gulya.isaeva.pls@gmail.com>
2024-08-09 15:32:25 +0300

    feat:add completed slider

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import Image from "next/image";
 function Featured() {
   return (
-    <div>Featured</div>
-  )
+    <div className="">
+      {/* Wrapper  */}
+      <div className="">
+        {/* Single Items */}
+        <div className="">
+          {/* Image Container */}
+          <Image src={""} alt={""} />
+        </div>
+        {/* Text Container */}
+      </div>
+    </div>
+  );
 }
 
-export default Featured
+export default Featured;
