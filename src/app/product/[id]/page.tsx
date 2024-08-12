b00cfdaec55f42094897e8d8d10e6c5a5160d625
Gulia <gulya.isaeva.pls@gmail.com>
2024-08-12 17:10:41 +0300

    feat:add order mobile size style and also products size functionality

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -6,20 +6,25 @@ import Price from "@/components/Price";
 function SingleProductPage() {
   return (
     <div className="p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-red-500 md:flex-row">
-      <div className="relative">
-        {singleProduct.img && (
+      {singleProduct.img && (
+        <div className="relative w-full h-1/2">
           <Image
             src={singleProduct.img}
             alt={""}
             className="object-contain"
             fill
           />
-        )}
-        <div className="">
-          <h1>{singleProduct.title}</h1>
-          <p>{singleProduct.desc}</p>
-          <Price price={singleProduct.price} id={singleProduct.id} options={singleProduct.options}/>
         </div>
+      )}
+
+      <div className="h-1/2 flex flex-col gap-4">
+        <h1 className="text-3xl font-bold uppercase">{singleProduct.title}</h1>
+        <p>{singleProduct.desc}</p>
+        <Price
+          price={singleProduct.price}
+          id={singleProduct.id}
+          options={singleProduct.options}
+        />
       </div>
     </div>
   );
--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 
 type Props = {
   price: number;
@@ -6,25 +7,69 @@ type Props = {
   options?: { title: string; additionalPrice: number }[];
 };
 function Price({ price, id, options }: Props) {
+  const [total, setTotal] = useState(price);
+  const [quantity, setQuantity] = useState(1);
+  const [selected, setSelected] = useState(0);
+
+  //   useEffect(() => {
+  //     console.log("Dependencies:", { quantity, selected, options, price });
+  //     setTotal(
+  //       quantity * (options ? price + options[selected].additionalPrice : price)
+  //     );
+  //   }, [quantity, selected, options, price]);
+  useEffect(() => {
+    setTotal(
+      quantity *
+        (options && options[selected]?.additionalPrice !== undefined
+          ? price + options[selected].additionalPrice
+          : price)
+    );
+  }, [
+    quantity,
+    selected,
+    options?.map((option) => option.additionalPrice).join(","),
+    price,
+  ]);
+
   return (
-    <div>
-      <h2>${price.toFixed(2)}</h2>
-      <div className="">
-        {options?.map((option) => (
-          <button key={option.title}>{option.title}</button>
+    <div className="flex flex-col gap-4">
+      <h2 className="text-2xl font-bold">${total.toFixed(2)}</h2>
+      <div className=" flex gap-4">
+        {options?.map((option, index) => (
+          <button
+            className="min-w-[6rem] ]p-2 ring-1 ring-red-400 rounded-md"
+            key={option.title}
+            style={{
+              background: selected === index ? "rgb(248 113 113)" : "white",
+              color: selected === index ? "white" : "red",
+            }}
+            onClick={() => setSelected(index)}
+          >
+            {option.title}
+          </button>
         ))}
       </div>
 
-      <div className="">
-        <div className="">
+      <div className=" flex justify-between items-center">
+        <div className="flex justify-between w-full p-3 ring-1 ring-red-500">
           <span>Quantity</span>
-          <div>
-            <button>{"<"}</button>
-            <span>1</span>
-            <button>{">"}</button>
+          <div className="flex gap-4 items-center">
+            <button
+              onClick={() => setQuantity((prev) => (prev > 1 ? prev - 1 : 1))}
+            >
+              {"<"}
+            </button>
+            <span>{quantity}</span>
+            <button
+              onClick={() => setQuantity((prev) => (prev < 9 ? prev + 1 : 9))}
+            >
+              {">"}
+            </button>
           </div>
         </div>
-        <button>Add to Cart</button>
+        <button className="uppercase w-56 bg-red-500 text-white p-3 ring-1 ring-red-500">
+          Add to Cart
+        </button>
       </div>
     </div>
   );
