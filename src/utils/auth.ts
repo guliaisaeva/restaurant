8272dd391bf730a0190a6a5bfe08880b009aab99
Gulia <gulya.isaeva.pls@gmail.com>
2024-08-13 17:59:23 +0300

    feat:add uncompleted auth and login

---
+++ src/utils/auth.ts
@@ -0,0 +1,13 @@
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+      //   clientId: process.env.GITHUB_ID as string,
+      //   clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+  ],
+};
