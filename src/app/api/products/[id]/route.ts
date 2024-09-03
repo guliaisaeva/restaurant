663f69025d819177ccf6b9905b5817d8aebd9376
Gulia <gulya.isaeva.pls@gmail.com>
2024-09-03 17:42:50 +0300

    delete functionality

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,3 +1,4 @@
+import { getAuthSession } from "@/utils/auth";
 import { prisma } from "@/utils/connect";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -22,4 +23,33 @@ export const GET = async (
       { status: 500 }
     );
   }
+};
+
+//dElete
+export const DELETE = async (
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  const { id } = params;
+  const session = await getAuthSession();
+  if (session?.user.isAdmin) {
+    try {
+      const product = await prisma.product.delete({
+        where: {
+          id: id,
+        },
+      });
+      return new NextResponse("Products has been deleted", {
+        status: 200,
+      });
+    } catch (error) {
+      return new NextResponse(
+        JSON.stringify({ message: "Something went wrong" }),
+        { status: 500 }
+      );
+    }
+  }
+  return new NextResponse(JSON.stringify({ message: "You are not allowed" }), {
+    status: 500,
+  });
 };
--- src/components/DeleteButton.tsx
+++ src/components/DeleteButton.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import Image from "next/image";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
-function DeleteButton() {
+function DeleteButton({ id }: { id: string }) {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  if (status === "loading") {
+    return <p>Loading...</p>;
+  }
+  if (status === "unauthenticated" || !session?.user.isAdmin) {
+    return;
+  }
+  const handleDelete =()=>{
+    
+  }
   return (
     <button className="bg-red-400 p-2 rounded-full absolute top-4 right-4">
-      <Image src="/images/delete.png" alt={""} width={20} height={20} />
+      <Image src="/images/delete.png" alt={""} width={20} height={20} onClick={handleDelete}/>
     </button>
   );
 }
