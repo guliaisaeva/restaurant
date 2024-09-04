7ed4d8b45de95fcbca914a4abc2dd728514f5f7d
Gulia <gulya.isaeva.pls@gmail.com>
2024-09-04 16:45:15 +0300

    feat:add payment components and functionality

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -2,12 +2,38 @@
 import React, { useEffect } from "react";
 import Image from "next/image";
 import { useCartStore } from "@/utils/store";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 function CartPage() {
   const { products, totalItems, totalPrice, removeFromCart } = useCartStore();
+  const { data: session } = useSession();
+  const router = useRouter();
   useEffect(() => {
     useCartStore.persist.rehydrate();
   }, []);
+  const handleCheckout = async () => {
+    if (!session) {
+      router.push("/");
+    } else {
+      try {
+        const res = await fetch("http://localhost:3000/api/orders", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            price: totalPrice,
+            products,
+            status: "Not Paid!",
+            userEmail: session.user.email,
+          }),
+        });
+        const data = await res.json();
+        router.push(`/pay/${data.id}`);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
   return (
     <div className=" h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-red-500 lg:flex-row">
       <div className="h-1/2  p-4 flex flex-col justify-center overflow-y-scroll lg:h-full lg:w-2/3 2xl:w-1/2 lg:px-20 xl:px-40">
@@ -54,7 +80,10 @@ function CartPage() {
           <span className="">TOTAL(INCL. VAT)</span>
           <span className="font-bold">${totalPrice}</span>
         </div>
-        <button className="uppercase bg-red-500 text-white p-3 rounded-md w-1/2 self-end">
+        <button
+          className="uppercase bg-red-500 text-white p-3 rounded-md w-1/2 self-end"
+          onClick={handleCheckout}
+        >
           Checkout
         </button>
       </div>
--- src/app/pay/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import React from "react";
-
-function PayPage() {
-  return <div>PayPage</div>;
-}
-
-export default PayPage;
