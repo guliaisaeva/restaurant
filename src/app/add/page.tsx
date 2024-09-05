1814a733cd447417928a001e36e982056add206f
Gulia <gulya.isaeva.pls@gmail.com>
2024-09-05 15:48:56 +0300

    feat:add product add functionality

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -2,23 +2,31 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
+
+type Inputs = {
+  title: string;
+  desc: string;
+  price: number;
+  catSlug: string;
+};
 type Option = {
   title: string;
   additionalPrice: number;
 };
 function AddPage() {
   const { data: session, status } = useSession();
-  const [inputs, setInputs] = useState({
+  const [inputs, setInputs] = useState<Inputs>({
     title: "",
     desc: "",
     price: 0,
-    catSlug: 0,
+    catSlug: "",
   });
   const [option, setOption] = useState<Option>({
     title: "",
     additionalPrice: 0,
   });
   const [options, setOptions] = useState<Option[]>([]);
+
   const router = useRouter();
   if (status === "loading") {
     return <p>Loading...</p>;
@@ -41,9 +49,28 @@ function AddPage() {
     });
   };
 
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch("http://localhost:3000/api/products", {
+        method: "POST",
+        body: JSON.stringify({ ...inputs, options }),
+      });
+
+      const data = await res.json();
+      console.log("data " + data);
+      //   router.push(`/product/${data.id}`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div>
-      <form className="shadow-lg flex flex-wrap gap-4 p-8">
+      <form
+        className="shadow-lg flex flex-wrap gap-4 p-8"
+        onSubmit={handleSubmit}
+      >
         <h1>Add New Product</h1>
         <div className="w-full flex flex-col gap-2">
           <label htmlFor="title">Title</label>
@@ -78,7 +105,7 @@ function AddPage() {
           <input
             className="ring-1 ring-red-200 rounded-sm"
             type="text"
-            name="category"
+            name="catSlug"
             id="category"
             onChange={handleChange}
           />
@@ -100,18 +127,26 @@ function AddPage() {
               name="additionalPrice"
               className="ring-1 ring-red-200 rounded-sm"
             />
-            <button
+            <div
               className="bg-gray-500 p-2 text-white"
-              //   onClick={() => setOptions((prev) => [...prev, option])}
+              onClick={() => setOptions((prev) => [...prev, option])}
             >
               Add Option
-            </button>{" "}
+            </div>{" "}
           </div>
-          <div className="flex flex-wrap gap-4 mt-2">
-            <div className="p-2 rounded-md cursor-pointer bg-gray-200 text-gray-400 flex gap-2">
-              <span>Small</span>
-              <span>$2</span>
-            </div>
+          <div className="flex  flex-wrap gap-4 mt-2">
+            {options.map((item) => (
+              <div
+                className="p-2 rounded-md cursor-pointer bg-gray-200 text-gray-400 flex gap-2"
+                key={item.title}
+                onClick={() =>
+                  setOptions(options.filter((opt) => opt.title !== item.title))
+                }
+              >
+                <span>{item.title}</span>
+                <span>${item.additionalPrice}</span>
+              </div>
+            ))}
           </div>
         </div>
         <button
